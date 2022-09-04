4cb0811ce9b2e60c0eccf24353ec25c63460b4ea
Your-Full-Name <your-email-address>
2022-09-04 18:48:28 +0800

    我提交了一个脚本

---
+++ name.py
@@ -0,0 +1,13 @@
+number = input('一个数字 ')
+name = input('一个人的名字')
+'''pet = input('一个宠物的名字 ')
+adj = input('一个形容词')
+sentence = input('手机不见了说的话 ')
+time = input('一段时间')
+thing = input('一件想做的事情 ')
+event = input('一个事件')
+fish = input('一个鱼的形容词')'''
+
+
+print('hello!', name , '这是我们相遇的第%s天' %number)
+
